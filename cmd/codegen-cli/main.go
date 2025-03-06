389ffc19fe7973c8d5da8f6323697033c2b7ce8b
Vyacheslav Tievsky <>
2025-03-06 06:20:24 +0300

    fix unknown command case

--- cmd/codegen-cli/main.go
+++ cmd/codegen-cli/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 
 	"github.com/urfave/cli/v2"
@@ -9,9 +9,28 @@ import (
 	httpserver "github.com/vtievsky/codegen-cli/cmd/codegen-cli/internal/http-server"
 )
 
+const (
+	ErrExitCode = 1
+)
+
 func main() {
+	var exitCode int
+
+	defer func() {
+		cli.OsExiter(exitCode)
+	}()
+
 	app := cli.NewApp()
-	app.DefaultCommand = "command-list"
+	app.Name = "codegen-cli"
+	app.Usage = "Codegen CLI"
+	app.Description = "Утилита кодогенерации"
+	app.Suggest = true
+
+	app.CommandNotFound = func(ctx *cli.Context, command string) {
+		fmt.Printf("The command was not found: %s\n", command)
+
+		exitCode = ErrExitCode
+	}
 
 	app.Commands = []*cli.Command{
 		httpserver.CommandGenerateHttpServer(),
@@ -20,6 +39,8 @@ func main() {
 	}
 
 	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+		exitCode = ErrExitCode
+
+		return
 	}
 }
